69e7f406d3af3c539548bbcb5179cf79d11e3d3e
Yannis Douros <yannis@douros.org>
2025-10-08 14:54:48 +0300

    Plumbed in OpenCV

--- cpp/fkcl/fkcl.cpp
+++ cpp/fkcl/fkcl.cpp
@@ -1,12 +1,28 @@
 ﻿// fkcl.cpp : Defines the entry point for the application.
 //
-
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
 #include "fkcl.h"
 
 using namespace std;
+using namespace cv;
 
 int main()
 {
 	listDevices();
+
+	int n_rows = 200;
+	int n_cols = 200;
+
+	cv::Mat fractalImage(n_rows, n_cols, CV_8UC3); 
+	fractalImage.setTo(cv::Scalar(0, 0, 0));
+
+
+
+	namedWindow("Display window", WINDOW_AUTOSIZE);
+	imshow("Display window", fractalImage);
+	waitKey(0);
+
+
 	return 0;
 }
